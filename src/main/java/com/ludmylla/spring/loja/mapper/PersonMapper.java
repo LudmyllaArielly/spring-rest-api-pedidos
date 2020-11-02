59a1da318f76944678b55ed0b5c4cf0c5ecc13e7
LudmyllaArielly <ludmay4@gmail.com>
2020-11-02 13:05:09 -0300

    Remoção de atributos entidade pessoa

--- src/main/java/com/ludmylla/spring/loja/mapper/PersonMapper.java
+++ src/main/java/com/ludmylla/spring/loja/mapper/PersonMapper.java
@@ -2,33 +2,25 @@ package com.ludmylla.spring.loja.mapper;
 
 import java.util.List;
 
-import com.ludmylla.spring.loja.dto.PersonInsertDto;
-import com.ludmylla.spring.loja.dto.PersonListDto;
-import com.ludmylla.spring.loja.model.Person;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
+import com.ludmylla.spring.loja.dto.PersonInsertDto;
+import com.ludmylla.spring.loja.dto.PersonListDto;
+import com.ludmylla.spring.loja.model.Person;
+
 @Mapper
 public interface PersonMapper {
-	
-    PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
 
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "raca", source = "cor")
-    @Mapping(target = "tamanho", source = "altura")
-    Person dtoInsetPerson(PersonInsertDto source);
-    
+	PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
 
-    @Mapping(target = "raca", source = "cor")
-    @Mapping(target = "tamanho", source = "altura")
-    @Mapping(target = "id", ignore = true)
-    @Mapping(target = "senha", ignore = true)
-    Person dtoListPerson(PersonListDto source);
-    
+	@Mapping(target = "id", ignore = true)
+	Person dtoInsetPerson(PersonInsertDto source);
 
-    @Mapping(target = "raca", source = "cor")
-    @Mapping(target = "tamanho", source = "altura")
-    List<Person> dtoListPersonList(List<PersonListDto> source);
+	@Mapping(target = "id", ignore = true)
+	Person dtoListPerson(PersonListDto source);
+
+	List<Person> dtoListPersonList(List<PersonListDto> source);
 
 }
--- src/main/java/com/ludmylla/spring/loja/model/Person.java
+++ src/main/java/com/ludmylla/spring/loja/model/Person.java
@@ -1,103 +1,77 @@
 
-  
 package com.ludmylla.spring.loja.model;
 
 import java.io.Serializable;
-import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-
 @Entity
 public class Person implements Serializable {
 
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String name;
+	private String cpf;
 
-    private String name;
-    private String cpf;
-    private String senha;
-    private String raca;
-    private Integer tamanho;
+	public Long getId() {
+		return id;
+	}
 
+	public void setId(Long id) {
+		this.id = id;
+	}
 
-    public Long getId() {
-        return id;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public String getName() {
-        return name;
-    }
+	public String getCpf() {
+		return cpf;
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
 
-    public String getCpf() {
-        return cpf;
-    }
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
 
-    public void setCpf(String cpf) {
-        this.cpf = cpf;
-    }
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
 
-    public String getSenha() {
-        return senha;
-    }
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", name=" + name + ", cpf=" + cpf + "]";
+	}
 
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
-
-    public String getRaca() {
-        return raca;
-    }
-
-    public void setRaca(String raca) {
-        this.raca = raca;
-    }
-
-    public Integer getTamanho() {
-        return tamanho;
-    }
-
-    public void setTamanho(Integer tamanho) {
-        this.tamanho = tamanho;
-    }
-
-    @Override
-    public String toString() {
-        return "Pessoa{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", cpf='" + cpf + '\'' +
-                ", senha='" + senha + '\'' +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Person pessoa = (Person) o;
-        return Objects.equals(id, pessoa.id) &&
-                Objects.equals(name, pessoa.name) &&
-                Objects.equals(cpf, pessoa.cpf) &&
-                Objects.equals(senha, pessoa.senha);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, cpf, senha);
-    }
 }
